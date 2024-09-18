0b75fbf897ceb190ade1e0c36ea0f3f00d7dbd7c
m.xoliyorov <m.xoliyorov@newuu.uz>
2024-09-18 12:47:16 +0500

    dasfaet

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+/*using namespace std;
+
+
+int main() {
+    int x;
+    cout <<"enter the integer: ";
+    cin >> x;
+    if (x>=0) {
+        cout << x << " is positive." << endl;
+    } else {
+        cout << x << " is negative." << endl;
+    }
+return 0;
+
+}*/
+/*include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    if ( a>b && a>c) { cout << a << " is the larget " << endl;}
+    if ( b>a && b>c) { cout << a << " is the larget " << endl;}
+    else { cout <<c<< " is the largest " << endl;}
+
+
+
+
+}*/
+
